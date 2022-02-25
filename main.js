5186646ecf95d5c5979efb020fe23ab2e81508f7
ji <jiyongprk@gmail.com>
2022-02-25 17:49:02 -0500

    testing things

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const btns = document.querySelectorAll('button');
+const documents = document.querySelector('.info');
 
 console.log(btns);
 
+let numA = 0;
+
+
+
 function tabs (){
     btns.forEach( btn => btn.addEventListener('click', ()=> {
-        console.log('clicked');
+        documents.textContent = 3;
+
+        numA = parseInt(documents.textContent);
+
+        console.log(numA);
     }))
        
 }
