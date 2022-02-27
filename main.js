31ff852322e227dc25196f840f657399534af8c2
ji <jiyongprk@gmail.com>
2022-02-27 17:34:21 -0500

    done

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ const infoBoard = document.querySelector('.info');
 
 historyBtns.addEventListener ('click', ()=>{
     infoBoard.textContent = "HISTORY Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
+    infoBoard.textContent += "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
 })
 
 
 visionBtns.addEventListener ('click', ()=>{
-    infoBoard.textContent = "VISION Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
+    infoBoard.textContent += "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
+    infoBoard.textContent += "VISION Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
 })
 
 
 goalsBtns.addEventListener ('click', ()=>{
     infoBoard.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
-    infoBoard.textContent += "afafaGOALS Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
+    infoBoard.textContent += "GOALS Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam explicabo cumque distinctio quia consectetur consequuntur delectus iure eius. Ea."
 })
